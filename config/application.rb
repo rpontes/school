04f2948139546246d26a31a1ec007a75bfc117cb
Ricardo Pontes <rpontes@gmail.com>
2013-02-14 15:38:13 +0100

    Include configuration for heroku

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module School
     config.active_record.whitelist_attributes = true
     config.assets.enabled = true
     config.assets.version = '1.0'
+    config.assets.initialize_on_precompile = false
   end
 end
